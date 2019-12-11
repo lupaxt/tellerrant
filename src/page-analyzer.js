4b79c2f67d4ee1a5b7e93abd5832c35d036a37da
Nikolas Martens <Nikolas.M@rtens.org>
2019-12-11 12:41:36 +0100

    Added function to extract heading from document

--- src/page-analyzer.js
+++ src/page-analyzer.js
@@ -2,7 +2,8 @@ function isItAnArticle(document) {
     let heuristics = [
         () => !!document.querySelectorAll('[itemtype^="http://schema.org/"], [itemtype$="Article"]').length, 	
         () => !!document.querySelectorAll('[property="og:type"], [content^="video"]').length,
-        () => !!document.querySelectorAll('[property="og:type"], [content^="article"]').length
+        () => !!document.querySelectorAll('[property="og:type"], [content^="article"]').length,
+        () => !!document.getElementsByTagName("h1").length
     ]
 
     for (var i=0; i<heuristics.length; i++) {
@@ -13,6 +14,16 @@ function isItAnArticle(document) {
     return false;
 }
 
+function extractHeading(document) {
+    const headings = document.getElementsByTagName("h1");
+    if (!headings.length) {
+        throw new Error("No Title found")
+    }
+
+    return headings[0].innerHTML;
+} 
+
 export { 
-    isItAnArticle
+    isItAnArticle,
+    extractHeading
 };
